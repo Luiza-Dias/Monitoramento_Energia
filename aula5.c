7832a46d1a4160bd0bf8cdb1b9fb7aa89d6dedfb
Luiza-Dias <66181468+Luiza-Dias@users.noreply.github.com>
2023-12-18 15:14:22 -0300

    Add files via upload

--- aula5.c
+++ aula5.c
@@ -4,15 +4,12 @@ CodeWizardAVR V2.05.0 Advanced
 Automatic Program Generator
 © Copyright 1998-2010 Pavel Haiduc, HP InfoTech s.r.l.
 http://www.hpinfotech.com
-
 Project : 
 Version : 
-Date    : 27/06/2013
+Date    : 20/06/2013
 Author  : www.Eca.ir *** www.Webkade.ir
 Company : 
 Comments: 
-
-
 Chip type               : ATmega8
 Program type            : Application
 AVR Core Clock frequency: 14,745600 MHz
@@ -22,12 +19,15 @@ Data Stack size         : 256
 *****************************************************/
 
 #include <mega16.h>
+
 #include <delay.h>
+
+// Standard Input/Output functions
 #include <stdio.h>
 
+#define ADC_VREF_TYPE 0x40 //Alteracao
 
-//leitura analogica:
-#define ADC_VREF_TYPE 0x40 
+// Read the AD conversion result
 unsigned int read_adc(unsigned char adc_input)
 {
 ADMUX=adc_input | (ADC_VREF_TYPE & 0xff);
@@ -41,177 +41,14 @@ ADCSRA|=0x10;
 return ADCW;
 }
 
-
-// I2C Bus functions
-#asm
-   .equ __i2c_port=0x15 ;PORTC
-   .equ __sda_bit=1
-   .equ __scl_bit=0
-#endasm
-#include <i2c.h>
-
-// DS1307 Real Time Clock functions
-#include <ds1307.h>
-
-#ifndef RXB8
-#define RXB8 1
-#endif
-
-#ifndef TXB8
-#define TXB8 0
-#endif
-
-#ifndef UPE
-#define UPE 2
-#endif
-
-#ifndef DOR
-#define DOR 3
-#endif
-
-#ifndef FE
-#define FE 4
-#endif
-
-#ifndef UDRE
-#define UDRE 5
-#endif
-
-#ifndef RXC
-#define RXC 7
-#endif
-
-#define FRAMING_ERROR (1<<FE)
-#define PARITY_ERROR (1<<UPE)
-#define DATA_OVERRUN (1<<DOR)
-#define DATA_REGISTER_EMPTY (1<<UDRE)
-#define RX_COMPLETE (1<<RXC)
-
-// USART Receiver buffer
-#define RX_BUFFER_SIZE 32
-char rx_buffer[RX_BUFFER_SIZE];
-
-#if RX_BUFFER_SIZE <= 256
-unsigned char rx_wr_index,rx_rd_index,rx_counter;
-#else
-unsigned int rx_wr_index,rx_rd_index,rx_counter;
-#endif
-
-// This flag is set on USART Receiver buffer overflow
-bit rx_buffer_overflow;
-bit rx_message; // Flag de recepção de mensagem
-
-// USART Receiver interrupt service routine
-interrupt [USART_RXC] void usart_rx_isr(void)
-{
-char status,data;
-status=UCSRA;
-data=UDR;
-if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
-   {
-   
-   if ( data == '\r' )
-    {
-      data = 0;
-      rx_message = 1;
-    }
-    
-   rx_buffer[rx_wr_index++]=data;
-#if RX_BUFFER_SIZE == 256
-   // special case for receiver buffer size=256
-   if (++rx_counter == 0)
-      {
-#else
-   if (rx_wr_index == RX_BUFFER_SIZE) rx_wr_index=0;
-   if (++rx_counter == RX_BUFFER_SIZE)
-      {
-      rx_counter=0;
-#endif
-      rx_buffer_overflow=1;
-      }
-   }
-}
-
-#ifndef _DEBUG_TERMINAL_IO_
-// Get a character from the USART Receiver buffer
-#define _ALTERNATE_GETCHAR_
-#pragma used+
-char getchar(void)
-{
-char data;
-while (rx_counter==0);
-data=rx_buffer[rx_rd_index++];
-#if RX_BUFFER_SIZE != 256
-if (rx_rd_index == RX_BUFFER_SIZE) rx_rd_index=0;
-#endif
-#asm("cli")
---rx_counter;
-#asm("sei")
-return data;
-}
-#pragma used-
-#endif
-
-// USART Transmitter buffer
-#define TX_BUFFER_SIZE 32
-char tx_buffer[TX_BUFFER_SIZE];
-
-#if TX_BUFFER_SIZE <= 256
-unsigned char tx_wr_index,tx_rd_index,tx_counter;
-#else
-unsigned int tx_wr_index,tx_rd_index,tx_counter;
-#endif
-
-// USART Transmitter interrupt service routine
-interrupt [USART_TXC] void usart_tx_isr(void)
-{
-if (tx_counter)
-   {
-   --tx_counter;
-   UDR=tx_buffer[tx_rd_index++];
-#if TX_BUFFER_SIZE != 256
-   if (tx_rd_index == TX_BUFFER_SIZE) tx_rd_index=0;
-#endif
-   }
-}
-
-#ifndef _DEBUG_TERMINAL_IO_
-// Write a character to the USART Transmitter buffer
-#define _ALTERNATE_PUTCHAR_
-#pragma used+
-void putchar(char c)
-{
-while (tx_counter == TX_BUFFER_SIZE);
-#asm("cli")
-if (tx_counter || ((UCSRA & DATA_REGISTER_EMPTY)==0))
-   {
-   tx_buffer[tx_wr_index++]=c;
-#if TX_BUFFER_SIZE != 256
-   if (tx_wr_index == TX_BUFFER_SIZE) tx_wr_index=0;
-#endif
-   ++tx_counter;
-   }
-else
-   UDR=c;
-#asm("sei")
-}
-#pragma used-
-#endif
-
-// Standard Input/Output functions
-#include <stdio.h>
-
 // Declare your global variables here
 
 void main(void)
 {
 // Declare your local variables here
-unsigned char hour, min, sec, date, month, year;
-  char message[32];
-  int i,j;
-  //char *strrec;
-  unsigned int valor_ad;
-  float Valor_tensao;
+unsigned int valor_ad;
+float valor_tensao;
+float valor_corrente;
 
 // Input/Output Ports initialization
 // Port B initialization
@@ -281,30 +118,21 @@ TIMSK=0x00;
 
 // USART initialization
 // Communication Parameters: 8 Data, 1 Stop, No Parity
-// USART Receiver: On
+// USART Receiver: Off
 // USART Transmitter: On
 // USART Mode: Asynchronous
 // USART Baud Rate: 19200
-
-
-
-
 UCSRA=0x00;
 UCSRB=0xD8;
 UCSRC=0x86;
 UBRRH=0x00;
-UBRRL=0x2f;
-
+UBRRL=0x5F;
 
 // Analog Comparator initialization
 // Analog Comparator: Off
 // Analog Comparator Input Capture by Timer/Counter 1: Off
 ACSR=0x80;
 SFIOR=0x00;
-
-// ADC initialization
-// ADC disabled
-ADCSRA=0x00;
 
 // ADC initialization
 // ADC Clock frequency: 921,600 kHz
@@ -319,50 +147,86 @@ SPCR=0x00;
 // TWI initialization
 // TWI disabled
 TWCR=0x00;
+//////////////////////////////////////////////////////////
+//https://github.com/Abdurraziq/ZMPT101B-arduino/blob/master/src/ZMPT101B.cpp
+/*int getZeroPoint()
+{
+	uint32_t Vsum = 0;
+	uint32_t measurements_count = 0;
+	uint32_t t_start = micros();
+	while (micros() - t_start < period)
+	{
+		Vsum += analogRead(pin);
+		measurements_count++;
+	}
+	return Vsum / measurements_count;
+}
+/// @brief Calculate root mean square (RMS) of AC valtage
+/// @param loopCount Loop count to calculate
+/// @return root mean square (RMS) of AC valtage
+float ZMPT101B::getRmsVoltage(uint8_t loopCount)
+{
+	double readingVoltage = 0.0f;
+	for (uint8_t i = 0; i < loopCount; i++)
+	{
+		int zeroPoint = this->getZeroPoint();
+		int32_t Vnow = 0;
+		uint32_t Vsum = 0;
+		uint32_t measurements_count = 0;
+		uint32_t t_start = micros();
+		while (micros() - t_start < period)
+		{
+			Vnow = analogRead(pin) - zeroPoint;
+			Vsum += (Vnow * Vnow);
+			measurements_count++;
+		}
+		readingVoltage += sqrt(Vsum / measurements_count) / ADC_SCALE * VREF * sensitivity;
+	}
+	return readingVoltage / loopCount;
+}
+*/
+//////////////////////////////////////////////////////////
 
-// I2C Bus initialization
-i2c_init();
 
-// DS1307 Real Time Clock initialization
-// Square wave output on pin SQW/OUT: Off
-// SQW/OUT pin state: 0
-rtc_init(0,0,0);
+printf("AT\r");
+delay_ms(2000);
+printf("AT+SAPBR=3,1,\"Contype\",\"GPRS\"\r");
+delay_ms(2000);
+//printf("AT+SAPBR=3,1,\"APN\",\"timbrasil.br\"");
+printf("AT+SAPBR=3,1,\"APN\",\"zap.vivo.com.br\"\r");
+delay_ms(2000);
+//printf("AT+SAPBR=3,1,\"USER\",\"tim\"");
+printf("AT+SAPBR=3,1,\"USER\",\"vivo\"\r");
+delay_ms(2000);
+//printf("AT+SAPBR=3,1,\"PWD\",\"tim\"");
+printf("AT+SAPBR=3,1,\"PWD\",\"vivo\"\r");
+delay_ms(2000);
+printf("AT+SAPBR=1,1\r");
+delay_ms(5000);
 
-
-
-  
 while (1)
-      {
-      // Place your code here   
-      valor_ad = read_adc(1);  //Alteracao
-      Valor_tensao=valor_ad*5.00/1024; //Alteracao
-      printf("Valor da Corrente=%f V\r\n",(Valor_tensao - 2.5)/0.066);   
-      rtc_get_time(&hour,&min,&sec);   
-      rtc_get_date(&date, &month, &year);
-      printf("\r\n%d:%d:%d:%d:%d:%d\r\n",hour,min,sec,date,month,year);
-      delay_ms(1000);
-      PORTB.0=~PORTB.0;   
-      if(rx_message)
-      {
-        rx_message=0;
-        for (i=0, j=rx_counter; i<j ; i++) 
-        {
-            message[i]=getchar();
-        }      
-        hour=(message[0]-48)*10+(message[1]-48);
-        min=(message[3]-48)*10+(message[4]-48);
-        sec=(message[6]-48)*10+(message[7]-48);  
-        
-        date=(message[10]-48)*10+(message[11]-48);  
-        month=(message[13]-48)*10+(message[14]-48);  
-        year=(message[16]-48)*10+(message[17]-48); 
-        
-        //hour=message[0];
-        //min=message[1];
-        //sec=message[2];        
-        printf("\r\nAJUSTANDO %d:%d:%d:%d:%d:%d\r\n",hour,min,sec,date,month,year);
-        rtc_set_time(hour,min,sec);  
-        rtc_set_date(date,month,year);
-      }
+      {    
+      
+      valor_ad = read_adc(1);
+      valor_tensao=valor_ad*5.00/1024;
+      valor_corrente = (valor_tensao - 2.4)/0.066;
+      
+      printf("AT+HTTPINIT\r");
+      delay_ms(2000);
+      printf("AT+HTTPPARA=\"TIMEOUT\",120\r");
+      delay_ms(2000);
+      printf("AT+HTTPSSL=0\r");
+      delay_ms(2000);
+      printf("AT+HTTPPARA=\"URL\",\"http://industrial.api.ubidots.com/api/v1.6/devices/monitoramento-energia/?token=BBUS-OD7MqFThSC1DocgNvL4qJkoe5BD6yB\"\r");
+      delay_ms(2000);
+      printf(" AT+HTTPPARA=\"CONTENT\",\"application/json\"\r");
+      delay_ms(2000);
+      printf("AT+HTTPDATA= 18,70000\r");
+      delay_ms(2000);
+      printf("{\"corrente\": %.2f}\r", valor_corrente);
+      delay_ms(2000);
+      printf("AT+HTTPACTION=1\r");
+      delay_ms(7000);
+
       }
 }
